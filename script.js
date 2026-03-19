6661ddad4c068a0462575fe258f7fbe913e1c292
K10-Dev <backupgtx3090@gmail.com>
2026-03-18 23:02:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function contato() {
   alert("Bora conversar 😎");
 }
-// document.addEventListener("mousemove", (e) => {
-//   document.body.style.setProperty("--x", e.clientX + "px");
-//   document.body.style.setProperty("--y", e.clientY + "px");
-// });
+
 document.addEventListener("mousemove", (e) => {
   document.body.style.setProperty("--x", e.clientX + "px");
   document.body.style.setProperty("--y", e.clientY + "px");
