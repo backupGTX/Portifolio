149a506a8d01dcdaa81f3d9a0e64845f8eb8692c
K10-Dev <backupgtx3090@gmail.com>
2026-03-18 22:58:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function contato() {
+  alert("Bora conversar 😎");
+}
+// document.addEventListener("mousemove", (e) => {
+//   document.body.style.setProperty("--x", e.clientX + "px");
+//   document.body.style.setProperty("--y", e.clientY + "px");
+// });
+document.addEventListener("mousemove", (e) => {
+  document.body.style.setProperty("--x", e.clientX + "px");
+  document.body.style.setProperty("--y", e.clientY + "px");
+});
